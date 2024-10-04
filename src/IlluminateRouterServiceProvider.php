853ded976cd72f6ba9d2166b0df3a94d55ebf520
moshabyte <164793433+moshabyte@users.noreply.github.com>
2024-10-05 00:33:43 +0600

    Add files via upload

---
+++ src/IlluminateRouterServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Illuminate\Router;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Route;
+class IlluminateRouterServiceProvider extends ServiceProvider
+{
+
+
+    public function register()
+    {
+       
+    }
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot(\Illuminate\Routing\Router $router)
+    {
+        
+        $this->loadRoutesFrom(__DIR__.'/routes/web.php');        
+        
+
+    }
+}
